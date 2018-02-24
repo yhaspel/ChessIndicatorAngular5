e45be82534767082eac445b29096e6dc4e6f63d3
yhaspel <yuval3000@gmail.com>
2018-02-24 15:31:13 +0200

    prep for heroku upload 2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/build'));
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
